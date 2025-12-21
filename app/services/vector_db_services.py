3da896fccfa70d37cd21c87831cf3a2eed331f46
Harsh Sharma <hs210310222360@gmail.com>
2025-12-20 23:53:40 -0500

    skeletal added for vector_db_services.py

---
+++ app/services/vector_db_services.py
@@ -0,0 +1,14 @@
+from ..schema import  RepoChunksResponse
+
+def get_client():
+    pass
+
+def get_collection():
+    pass
+
+def store_repo_embedding(repo_name: str, chunks: RepoChunksResponse):
+    pass
+
+def search_repo(repo_name: str, chunk_id: int, content: str, top_k : int = 5):
+    pass
+
